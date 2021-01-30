f62369174fa10f1ec193bf7de87fd0397adb0fc6
Fluqz <robin@hlawinka.de>
2021-01-30 18:16:00 +0100

    hand

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ const scene = new Phaser.Scene('game')
 const config = {
 
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: window.innerWidth,
+    height: window.innerHeight,
     physics: {
         default: 'arcade',
         arcade: {
@@ -28,32 +28,31 @@ scene.init = function() {
 
 scene.preload = function() {
 
-    this.load.setBaseURL('http://labs.phaser.io');
+    this.load.setBaseURL('./');
 
-    this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('handopen', 'assets/sprites/phaser3-logo.png');
-    this.load.image('handclosed', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
+    this.load.image('handclosed', 'assets/hand-closed.png');
+    this.load.image('handopen', 'assets/hand-open.png');
 };
 
 scene.create = function() {
-
-    this.player = this.add.image(100, 100, 'handopen')
-    this.player = this.add.image(100, 100, 'handclosed')
+    this.player = this.add.sprite(100, 100, 'handopen')
+    this.player.scale = .5
 };
 
 scene.update = function() {
     // Is mouse click down?
         // move player along the x-axis at a rate this.speed pixels
 
-
     if (this.input.activePointer.isDown) {
 
         // this.player = this.add.image(100, 100, 'hand-closed')
         // hand closed
-        console.log(this.player.image = '');
+
+        this.player.setTexture('handclosed')
+
     }
     else {
+        this.player.setTexture('handopen')
 
         // this.player = this.add.image(100, 100, 'hand-open')
         // Hand open
---
+++ player.js
@@ -0,0 +1,15 @@
+
+
+
+export class Player {
+
+    sprite1
+    sprite2
+
+    constructor(type) {
+
+        this.sprite1 = new Phaser.GameObjects.Sprite()
+
+    }
+    
+}
