750d187b9670dcf22967135c3861d13f66ffc8d1
Fluqz <robin@hlawinka.de>
2021-01-30 21:04:05 +0100

    stuff

--- player.js
+++ player.js
@@ -8,10 +8,11 @@ export class Player {
 
     constructor() {
 
-        this.sprite = Greifer.scene.matter.add.sprite(100, 100, 'handopen')
+        this.sprite = Greifer.scene.matter.add.sprite(100, 100, 'handopen', )
         this.sprite.scale = .5
         this.sprite.setRectangle(100, 100)
         this.sprite.setDensity(1)
+        this.sprite.setFrictionAir(.05)
     }
  
     
@@ -24,6 +25,7 @@ export class Player {
         if (y > window.innerHeight / 2) {
             this.sprite.y = y
         }
+        else this.sprite.y = window.innerHeight / 2
 
     }
 
