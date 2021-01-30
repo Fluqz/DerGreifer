1fa530823e667b29a6d03dd94e270130e2e41cac
Patrick <baesslerpa@gmail.com>
2021-01-30 21:04:57 +0100

    hitbox json

--- player.js
+++ player.js
@@ -5,12 +5,14 @@ import { Greifer } from "./main.js"
 export class Player {
 
     sprite
+    hitbox
 
     constructor() {
 
         this.sprite = Greifer.scene.matter.add.sprite(100, 100, 'handopen')
+        
         this.sprite.scale = .5
-        this.sprite.setRectangle(100, 100)
+        this.sprite.setPolygon('40 0 40 20 100 20 100 80 40 80 40 100 0 50')
     }
  
     
